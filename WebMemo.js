a0d6aba6dd16b19b02da01942081dda4a31a52e3
syoung <syoung1394@naver.com>
2017-12-09 14:26:13 +0900

    save

--- WebMemo.js
+++ WebMemo.js
@@ -1,5 +1,16 @@
 window.onload=function init(){
-  var display=document.getElementById("display");
-  localStorage.getItem("title");
-
+  for(var i=0; i<localStorage.length; i++){
+    var key=localStorage.key(i);
+    var value=localStorage.getItem(key);
+    showList(value);
+  }
+}
+function showList(value){
+  var show=document.getElementById("show");
+  var li=document.createElement("li");
+  var span=document.createElement("span");
+  span.setAttribute("class","memo");
+  span.innerHTML=value;
+  li.appendChild(span);
+  show.appendChild(li);
 }
--- addMemo.js
+++ addMemo.js
@@ -1,16 +1,9 @@
 window.onload=function init(){
-var save=document.getElementById("save");
+var save=document.getElementById("save_btn");
 save.onclick=saveContent;
-var list=document.getElementById("list");
-list.onclick=showList;
 }
 function saveContent(){
   var key = document.getElementById("title").value;
   var value=document.getElementById("content").value;
   localStorage.setItem(key,value);
-  showList(value);
-}
-function showList(value){
-  var show=document.getElementById("show");
-  show.innerHTML=value;
 }
