6d048f6eedde5f500e00b825020e2deda289e86d
syoung <syoung1394@naver.com>
2017-12-09 17:42:14 +0900

    add delete function

--- WebMemo.js
+++ WebMemo.js
@@ -2,15 +2,29 @@ window.onload=function init(){
   for(var i=0; i<localStorage.length; i++){
     var key=localStorage.key(i);
     var value=localStorage.getItem(key);
-    showList(value);
+    showList(key,value);
   }
 }
-function showList(value){
+function showList(key,value){
   var show=document.getElementById("show");
-  var li=document.createElement("li");
+  var memo=document.createElement("li");
+  memo.setAttribute("id",key);
   var span=document.createElement("span");
   span.setAttribute("class","memo");
   span.innerHTML=value;
-  li.appendChild(span);
-  show.appendChild(li);
+  memo.appendChild(span);
+  show.appendChild(memo);
+  memo.onclick=deleteMemo;
+}
+function deleteMemo(e){
+  var key=e.target.id;
+  if(e.target.tagName.toLowerCase()=="span"){
+    key=e.target.parentNode.id;
+  }
+  localStorage.removeItem(key);
+  removeMemoFromDom(key);
+}
+function removeMemoFromDom(key){
+  var memo=document.getElementById(key);
+  memo.parentNode.removeChild(memo);
 }
--- addMemo.js
+++ addMemo.js
@@ -3,7 +3,7 @@ var save=document.getElementById("save_btn");
 save.onclick=saveContent;
 }
 function saveContent(){
-  var key = document.getElementById("title").value;
+  var key = localStorage.length;
   var value=document.getElementById("content").value;
   localStorage.setItem(key,value);
 }
